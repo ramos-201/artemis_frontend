1f7cde2da9e1d2bc7f598e96712902ef7c7ffab8
Ramos <andfera201@gmail.com>
2025-02-13 20:55:57 -0500

    fix: "Material UI" library implemented for styles
    - `@mui/material`, `@emotion/react` and `@emotion/styled` were
      installed.
    - Basic styles added to my login page.
    - Tests have been modified for the new settings.

--- __tests__/pages/login.test.tsx
+++ __tests__/pages/login.test.tsx
@@ -6,25 +6,24 @@ import userEvent from "@testing-library/user-event";
 describe("Login Page", () => {
   beforeEach((): void => {
     render(<LoginPage />);
-  })
+  });
 
   test("It should correctly display the form elements", () => {
     const form: HTMLElement = screen.getByTestId("login-form");
     expect(within(form).getByRole("heading", { level: 2 })).toHaveTextContent(/^Iniciar Sesión$/);
-    expect(within(form).getByLabelText("Email")).toBeInTheDocument();
-    expect(within(form).getByLabelText("Password")).toBeInTheDocument();
-    expect(within(form).getByLabelText("Password")).toBeInTheDocument();
+    expect(within(form).getByRole("textbox", { name: /Nombre de usuario/i })).toBeInTheDocument();
+    expect(within(form).getByLabelText(/Contraseña/i)).toBeInTheDocument();
     expect(within(form).getByRole("button", { name: /Iniciar Sesión/i })).toBeInTheDocument();
   });
 
   test("Must allow data entry into the form", async () => {
-    const emailInput: HTMLElement = screen.getByLabelText(/Email/i);
-    const passwordInput: HTMLElement = screen.getByLabelText(/Password/i);
+    const emailInput: HTMLElement = screen.getByLabelText(/Nombre de usuario/i);
+    const passwordInput: HTMLElement = screen.getByLabelText(/Contraseña/i);
 
     await userEvent.type(emailInput, "jhon.smith@example.com");
     await userEvent.type(passwordInput, "password_example");
 
     expect(emailInput).toHaveValue("jhon.smith@example.com");
     expect(passwordInput).toHaveValue("password_example");
-  })
+  });
 })
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,34 +1,39 @@
-import {ChangeEvent, JSX, useState} from "react";
+import { ChangeEvent, JSX, useState } from "react";
+import {Box, Button, TextField, Typography} from "@mui/material";
 
 function LoginPage(): JSX.Element {
-  const [emailForm, setEmailForm] = useState<string>("");
+  const [usernameForm, setUsernameForm] = useState<string>("");
   const [passwordForm, setPasswordForm] = useState<string>("");
 
   return (
     <>
       <form data-testid="login-form">
-        <h2>Iniciar Sesión</h2>
-        <div>
-          <label htmlFor="username-form">Email</label>
-          <input
+        <Typography component="h2">Iniciar Sesión</Typography>
+        <Box>
+          <TextField
             id="username-form"
+            label="Nombre de usuario"
             type="text"
+            variant="outlined"
+            fullWidth
             required
-            value={emailForm}
-            onChange={(e: ChangeEvent<HTMLInputElement>): void => setEmailForm(e.target.value)}
+            value={usernameForm}
+            onChange={ (e: ChangeEvent<HTMLInputElement>): void => setUsernameForm(e.target.value) }
           />
-        </div>
-        <div>
-          <label htmlFor="password-form">Password</label>
-          <input
+        </Box>
+        <Box>
+          <TextField
             id="password-form"
+            label="Contraseña"
             type="password"
+            variant="outlined"
+            fullWidth
             required
-            value={passwordForm}
-            onChange={(e: ChangeEvent<HTMLInputElement>): void => setPasswordForm(e.target.value)}
+            value={ passwordForm }
+            onChange={ (e: ChangeEvent<HTMLInputElement>): void => setPasswordForm(e.target.value) }
           />
-        </div>
-        <button type="submit">Iniciar Sesión</button>
+        </Box>
+        <Button type="submit" variant="contained">Iniciar Sesión</Button>
       </form>
     </>
   )
